3d7748d88feb2bd8fa06e9dcf5be7427ee036651
Matthew van Heerden <matt.v.h152342@gmail.com>
2026-05-11 14:56:08 +0200

    Made basic Layout and add Book function

---
+++ script.js
@@ -0,0 +1,13 @@
+const library = []
+
+function Book(name, author, id) {
+    this.name = name;
+    this.author = author;
+    this.id = id;
+}
+
+Book.prototype.addBookToLibrary = function (bookName, bookAuthor, bookID) {
+    const book = new Book(bookName, bookAuthor, bookID);
+    library.push(book);
+    console.log("Book added");
+}
