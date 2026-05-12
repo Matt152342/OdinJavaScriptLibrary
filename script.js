81b869ab5e079aad25ef1d7087d198ceb598cb49
Matthew van Heerden <matt.v.h152342@gmail.com>
2026-05-12 15:31:58 +0200

    Updated page style and added function to display books

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 const library = []
 
-function Book(name, author, id) {
-    this.name = name;
+function Book(title, author, numberOfPages) {
+    this.title = title;
     this.author = author;
-    this.id = id;
+    this.numberOfPages = numberOfPages;
+    this.id = crypto.randomUUID();
 }
 
-Book.prototype.addBookToLibrary = function (bookName, bookAuthor, bookID) {
-    const book = new Book(bookName, bookAuthor, bookID);
+function addBook(bookTitle, bookAuthor, numberOfPages) {
+    const book = new Book(bookTitle, bookAuthor, numberOfPages);
     library.push(book);
     console.log("Book added");
 }
 
+for(let i = 0; i < 2; i++) {
+    addBook("Hunger Games", "Mary Poppins");
+}
+
+function createCard(book) {
+    const bookGrid = document.getElementById('bookGrid');
+    const bookCard = document.createElement('div');
+    const bookTitle = document.createElement('p');
+    const bookAuthor = document.createElement('p');
+    const bookID = document.createElement('p');
+    
+    bookCard.classList.add('bookCard');
+
+    bookTitle.textContent = `${book.title}`;
+    bookAuthor.textContent = `${book.author}`;
+    bookID.textContent = `${book.id}`;
+
+    bookGrid.appendChild(bookCard);
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookID);
+}
+
+function displayBooks() {
+    for (let i = 0; i < library.length; i++) {
+        createCard(library[i]);
+    }
+}
+
+displayBooks();
