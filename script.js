fe6d044cdc84ffbfae5b4ec064a42e3f6b541198
Matthew van Heerden <matt.v.h152342@gmail.com>
2026-05-13 20:08:07 +0200

    Added remove function

--- script.js
+++ script.js
@@ -13,33 +13,66 @@ function addBook(bookTitle, bookAuthor, numberOfPages) {
     console.log("Book added");
 }
 
-for(let i = 0; i < 2; i++) {
-    addBook("Hunger Games", "Mary Poppins");
-}
-
 function createCard(book) {
     const bookGrid = document.getElementById('bookGrid');
     const bookCard = document.createElement('div');
     const bookTitle = document.createElement('p');
     const bookAuthor = document.createElement('p');
-    const bookID = document.createElement('p');
+    const bookPages = document.createElement('p');
+    const removeBtn = document.createElement('button');
     
     bookCard.classList.add('bookCard');
-
     bookTitle.textContent = `${book.title}`;
-    bookAuthor.textContent = `${book.author}`;
-    bookID.textContent = `${book.id}`;
+    bookAuthor.textContent = `By ${book.author}`;
+    bookPages.textContent = `${book.numberOfPages}`;
+    removeBtn.textContent = 'Remove';
+    removeBtn.classList.add('removeBook');
+    removeBtn.setAttribute('id', book.id);
 
     bookGrid.appendChild(bookCard);
     bookCard.appendChild(bookTitle);
     bookCard.appendChild(bookAuthor);
-    bookCard.appendChild(bookID);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(removeBtn);
 }
 
 function displayBooks() {
-    for (let i = 0; i < library.length; i++) {
-        createCard(library[i]);
-    }
+    const bookGrid = document.getElementById('bookGrid');
+    bookGrid.innerHTML = "";
+
+    library.forEach((book) => {
+        createCard(book);
+    });
 }
 
-displayBooks();
+const form = document.getElementById('bookForm');
+const bookDialog = document.getElementById('formContainer');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+
+    addBook(title, author, pages);
+
+    displayBooks(); 
+    
+    bookDialog.close();
+    form.reset();
+})
+
+const bookGrid = document.getElementById('bookGrid');
+bookGrid.addEventListener('click', (event) => {
+    if (event.target.classList.contains('removeBook')) {
+        const bookID = event.target.id;
+
+        for(let i = 0; i < library.length; i++) {
+            if (bookID === library[i].id) {
+                library.splice(i, 1);
+                break;
+            }
+        }
+        displayBooks();
+    }
+});
